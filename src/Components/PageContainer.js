83ea704a9653c17bf7f0a53db883436f216c9057
Kennan Smith <kennan.smith1@gmail.com>
2019-05-15 14:05:45 -0600

    project is being scrapped - original plans proved to be more difficult than what is worthwhile for this project - I will start over in a new repository titled 'React_Portfolio'

---
+++ src/Components/PageContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+// import other components
+
+
+export default class PageContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state
+    }
+}
